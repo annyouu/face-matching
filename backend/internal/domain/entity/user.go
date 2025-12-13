255f722f122d70f8feaf67405040ba366d73cac3
antou <andong041205@gmail.com>
2025-12-14 01:01:35 +0900

    [fix] users修正

--- backend/internal/domain/entity/user.go
+++ backend/internal/domain/entity/user.go
@@ -5,7 +5,7 @@ import "time"
 type User struct {
 	ID        string `json:"id"`
 	Email     string `json:"email"`
-	Password  string `json:"password"`
+	PasswordHash  string `json:"password_hash"`
 	Name string `json:"name"`
 	CreatedAt time.Time `json:"created_at"`
 	UpdatedAt time.Time `json:"updated_at"`
