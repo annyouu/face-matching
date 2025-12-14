0b2630aff25b4febecf858b91e867bef3d986da2
antou <andong041205@gmail.com>
2025-12-14 18:03:30 +0900

    [add] usecase実装

--- backend/internal/usecase/user_usecase.go
+++ backend/internal/usecase/user_usecase.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"time"
 
+	"destinyface/internal/domain"
 	"destinyface/internal/domain/entity"
 	"destinyface/internal/domain/repository"
 	"destinyface/internal/usecase/dto"
@@ -14,7 +15,7 @@ import (
 	"golang.org/x/crypto/bcrypt"
 )
 
-type UserUsecaseInterface interface {
+type UserUseCaseInterface interface {
 	// 新規登録
 	Register(ctx context.Context, input *dto.UserRegisterInput) (*dto.UserOutput, error)
 	// ログイン
@@ -23,5 +24,59 @@ type UserUsecaseInterface interface {
 	GetProfile(ctx context.Context, userID string) (*dto.UserOutput, error)
 	// プロフィール更新
 	UpdateProfile(ctx context.Context, userID string, input *dto.UserUpdateInput) (*dto.UserOutput, error)
+}
+
+type UserUseCase struct {
+	userRepo repository.UserRepositoryInterface
+}
+
+// コンストラクタ
+// func NewUserUseCase(userRepo repository.UserRepositoryInterface) UserUseCaseInterface {
+// 	return &UserUseCase{
+// 		userRepo: userRepo
+// 	}
+// }
+
+// 新規登録
+func (u *UserUseCase) Register(ctx context.Context, input *dto.UserRegisterInput) (*dto.UserOutput, error) {
+
+	// いずれかの入力値が空かどうか
+	if input.Name == "" || input.Email == "" || input.Password == "" {
+		return nil, domain.ErrInvalidInput
+	}
+
+	// Email形式の正規化チェック(詳細なバリデーション)
+
+	// 入力値がすでにあるか重複しているかどうか
+
+	// OKだったら、パスワードのハッシュかを行う
+
+
+	// エンティティの生成
+
+	// リポジトリを呼び出し、DBへ保存する
+
+	// 成功レスポンスを返す
+}
+
+// ログイン
+func (u *UserUseCase) Login() {
+	// 401
+
+	// 403
+
+	// 404
+
+	// 400
+}
+
+// プロフィール取得
+func (u *UserUseCase) GetProfile() {
+	
+}
+
+// プロフィール更新
+func (u *UserUseCase) UpdateProfile() {
+	
 }
 
