2b77b8d726ae51decd97dd4b7f61ca43fb709319
antou <andong041205@gmail.com>
2026-01-15 13:00:25 +0900

    update: Status追加

--- backend/internal/domain/entity/user.go
+++ backend/internal/domain/entity/user.go
@@ -8,6 +8,7 @@ type User struct {
 	PasswordHash  string `json:"password_hash"`
 	Name string `json:"name"`
 	ProfileImageURL string
+	Status string
 	CreatedAt time.Time `json:"created_at"`
 	UpdatedAt time.Time `json:"updated_at"`
 }
