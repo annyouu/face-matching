ab95c2c6b56ac785426f56409bafe568439c92af
antou <andong041205@gmail.com>
2026-01-26 21:37:09 +0900

    update: ルーティング変更

--- frontend/features/routes/setup/endpoint.ts
+++ frontend/features/routes/setup/endpoint.ts
@@ -4,9 +4,9 @@ import { UserResponse } from "@/type/user";
 export const setupEndpoints = {
   // 名前を設定する
   updateName: (name: string) =>
-    apiClient.patch<UserResponse>("/api/v1/users/setup/name", { name }),
+    apiClient.patch<UserResponse>("/users/setup/name", { name }),
 
   // 画像設定
   updateImage: (imageUrl: string) =>
-    apiClient.patch<UserResponse>("/api/v1/users/setup/image", { profile_image_url: imageUrl }),
+    apiClient.patch<UserResponse>("/users/setup/image", { profile_image_url: imageUrl }),
 };
