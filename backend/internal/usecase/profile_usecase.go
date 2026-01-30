e7d5b424b1a446783887db45ec6fe6776e901a61
antou <andong041205@gmail.com>
2026-01-30 18:46:11 +0900

    [add] profile_usecaseを作成した

---
+++ backend/internal/usecase/profile_usecase.go
@@ -0,0 +1,63 @@
+package usecase
+
+import (
+	"context"
+	"io"
+	"destinyface/internal/domain/repository"
+	"destinyface/internal/usecase/dto"
+)
+
+type ProfileUseCaseInterface interface {
+	SetupProfileImage(ctx context.Context, userID string, file io.Reader) (*dto.UserOutput, error)
+}
+
+type profileUseCase struct {
+	userRepo repository.UserRepositoryInterface
+	fileRepo repository.FileStorageInterface
+}
+
+// コンストラクタで初期化処理
+func NewProfileUseCase(
+	ur repository.UserRepositoryInterface,
+	fr repository.FileStorageInterface,
+) ProfileUseCaseInterface {
+	return &profileUseCase{
+		userRepo: ur,
+		fileRepo: fr,
+	}
+}
+
+func (u *profileUseCase) SetupProfileImage(ctx context.Context, userID string, file io.Reader) (*dto.UserOutput, error) {
+	// 画像を送ったユーザー名を取得
+	user, err := u.userRepo.FindByID(ctx, userID)
+	if err != nil {
+		return nil, err
+	}
+
+	// S3へ保存する
+	uploadPath := user.GenerateProfilePath()
+	imageURL, err := u.fileRepo.Upload(ctx, file, uploadPath)
+	if err != nil {
+		return nil, err
+	}
+
+	// Entityのルールを適用し、画像保存できたので、ステータスをACTIVEにする
+	if err := user.CompleteProfileSetup(imageURL); err != nil {
+		return nil, err
+	}
+
+	// DBを更新する
+	if err := u.userRepo.Update(ctx, user); err != nil {
+		return nil, err
+	}
+
+	// レスポンスを返す
+	return &dto.UserOutput{
+		ID: user.ID,
+		Name: user.Name,
+		Email: user.Email,
+		ProfileImageURL: user.ProfileImageURL,
+		Status: user.Status,
+		CreatedAt: user.CreatedAt,
+	}, nil
+}
