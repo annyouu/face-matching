ce5b6e08a86d754cbaf9f925f78059c7514cd87c
antou <andong041205@gmail.com>
2026-02-06 20:48:02 +0900

    update: コメント削除

--- frontend/src/features/auth/api/Register.ts
+++ frontend/src/features/auth/api/Register.ts
@@ -3,8 +3,6 @@ import type { UserRegisterInput, UserResponse } from "../types";
 
 /**
  * 新規登録リクエストを送る関数
- * @param data { email, password }
- * @returns サーバーからのレスポンス（token, statusなど）
  */
 
 export const registerUser = (data: UserRegisterInput): Promise<UserResponse> => {
